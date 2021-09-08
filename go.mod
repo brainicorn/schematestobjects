ccb352399b180db334b38c829add75b4e211db25
Jonathan Doklovic <doklovic@atlassian.com>
2021-09-08 15:00:33 -0500

    added go module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/brainicorn/schematestobjects
+
+go 1.17
+
+require github.com/brainicorn/to v0.0.0-20210908195914-03b921a793c5
--- vendor/github.com/brainicorn/to/to.go
+++
@@ -1,8 +0,0 @@
-package to
-
-type Title struct {
-	// @jsonSchema(required=true)
-	ShortName string
-	// @jsonSchema(required=true)
-	LongName string
-}
